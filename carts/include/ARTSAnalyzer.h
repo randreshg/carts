f1dab40123dae304509305ff6047af706982c27e
randreshg <randres2011@gmail.com>
2024-05-05 02:15:17 +0000

    Add ARTSIRBuilder to CARTS.cpp

--- carts/include/ARTSAnalyzer.h
+++ carts/include/ARTSAnalyzer.h
@@ -6,37 +6,43 @@
 #include "llvm/IR/Value.h"
 #include "llvm/Transforms/IPO/Attributor.h"
 #include <cstdint>
+#include <sys/types.h>
+
+#include "ARTSIRBuilder.h"
 
 using namespace llvm;
 namespace arts {
 struct ARTSAnalyzer {
-  ARTSAnalyzer(AnalysisGetter AG) : AG(AG) {}
-  ARTSAnalyzer() {}
+  // ARTSAnalyzer(AnalysisGetter AG) : AG(AG) {}
+  ARTSAnalyzer(ARTSIRBuilder &AIB) : AIB(AIB) {}
 
-  /// Analyzes the Module to identify the OpenMP regions
-  uint64_t getNumberofOpenMPRegions(Module &M);
-
+  /// This function identifies the EDTs in the function
+  bool identifyEDTs(Function &F);
+  /// Analyze EDTs
+  void analyzeDeps();
   /// Analyzes the outlined region, replaces the RT call with a call to the
   /// outlined function, which is also modified to remove the arguments that
   /// are not needed.
   bool handleParallelOutlinedRegion(CallBase *RTCall);
-
   /// Analyzes Task region
   bool handleTaskOutlinedRegion(CallBase *CB);
-
   /// Analyzes the done region and return next BB to analyze
-  BasicBlock *handleDoneRegion(BasicBlock *DoneBB, DominatorTree *DT, 
+  BasicBlock *handleDoneRegion(BasicBlock *DoneBB, DominatorTree *DT,
                                std::string PrefixName, std::string SuffixBB);
+  /// EDTs
+  uint64_t getNumEDTs();
 
-  /// This function identifies the EDTs in the function
-  bool identifyEDTs(Function &F);
-
-  /// Analyze EDTs
-  void analyzeDeps();
+private:
+  EDT *createEDT(EDT::Type Ty, Module &M);
+  void insertEDT(EDT *E);
+  void removeEDT(EDT *E);
 
   /// Attributes
-  AnalysisGetter AG;
+  // AnalysisGetter AG;
+  ARTSIRBuilder &AIB;
+  /// Set of EDTs
+  SetVector<EDT *> EDTs;
 };
-}
+} // namespace arts
 
 #endif // LLVM_ARTS_ANALYZER_H
--- carts/include/ARTSUtils.h
+++ carts/include/ARTSUtils.h
@@ -1,23 +1,21 @@
 #ifndef LLVM_ARTS_UTILS_H
 #define LLVM_ARTS_UTILS_H
 
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "llvm/Transforms/Utils/CodeExtractor.h"
-#include "llvm/Transforms/Utils/ValueMapper.h"
-#include "llvm/Transforms/Utils/Cloning.h"
-#include "llvm/Transforms/Utils/ModuleUtils.h"
-#include "llvm/Analysis/AssumptionCache.h"
 #include "noelle/core/Noelle.hpp"
-#include "llvm/Analysis/CallGraph.h"
-
+#include "llvm/Analysis/AssumptionCache.h"
+// #include "llvm/Analysis/CallGraph.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/CodeExtractor.h"
+#include "llvm/Transforms/Utils/ModuleUtils.h"
+#include "llvm/Transforms/Utils/ValueMapper.h"
 
 namespace arts {
 using BlockSequence = SmallVector<BasicBlock *, 0>;
 
 /// This function finds the inputs and outputs of a region.
 void getInputsOutputs(BlockSequence Region, DominatorTree *DT,
-                      SetVector<Value *> &Inputs,
-                      SetVector<Value *> &Outputs,
+                      SetVector<Value *> &Inputs, SetVector<Value *> &Outputs,
                       SetVector<Value *> &Sinks);
 
 /// It finds the BBs that are dominated by FromBB and add
@@ -27,36 +25,34 @@ void getDominatedBBs(BasicBlock *FromBB, DominatorTree &DT,
 
 /// This function finds the calls that are dominated by CB and add
 /// them to the Calls vector.
-void getDominatedCalls(CallBase *CB, DominatorTree *DT, 
+void getDominatedCalls(CallBase *CB, DominatorTree *DT,
                        SmallSetVector<CallBase *, 16> &Calls);
 
 /// Remove values interface
-void removeValue(Value *V, bool RecursiveRemove = false,
-                 bool RecursiveUndef = true,  Instruction *Exclude = nullptr);
+void removeValue(Value *V, bool RecursiveRemove, bool RecursiveUndef,
+                 Instruction *Exclude);
 
 void removeValues(SmallVector<Value *, 16> ValuesToRemove);
 
-/// Function to replace uses of a Value with UndefValue. 
+/// Function to replace uses of a Value with UndefValue.
 /// - Instructions can be removed if requested.
 /// - The processs can also be performed in a recursive way by replacing
 ///   uses of the instructions that use the value with UndefValue.
-void replaceValueWithUndef(Value *V, bool RemoveInsts = false, 
-                           bool Recursive = true, Instruction *Exclude = nullptr);
+void replaceValueWithUndef(Value *V, bool RemoveInsts, bool Recursive,
+                           Instruction *Exclude);
 
 /// Given a basic block, this function creates a function that contains
 /// the instructions of the BB. The function is then inserted to the
 /// module.
-Function *
-createFunction(DominatorTree *DT, BasicBlock *FromBB,
-                bool DTAnalysis = false, std::string FunctionName = "",
-                SmallVector<Value *, 0> *ExcludeArgsFromAggregate = nullptr);
+Function *createFunction(DominatorTree *DT, BasicBlock *FromBB, bool DTAnalysis,
+                         std::string FunctionName,
+                         SmallVector<Value *, 0> *ExcludeArgsFromAggregate);
 
 /// Removes the lifetime markers from the function.
 void removeLifetimeMarkers(Function &F);
 
-
 namespace omp {
-/// OMP INFO 
+/// OMP INFO
 /// Helper Struct to get OpenMP related information
 struct OMPInfo {
   enum RTFType {
@@ -119,7 +115,7 @@ struct OMPInfo {
     return false;
   }
 };
-}
-}
+} // namespace omp
+} // namespace arts
 
 #endif // LLVM_ARTS_UTILS_H
