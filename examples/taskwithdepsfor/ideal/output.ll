644929c346274969fca35b1e5c89487815119293
randreshg <randres2011@gmail.com>
2024-12-25 15:11:32 -0500

    working ideal example

---
+++ examples/taskwithdepsfor/ideal/output.ll
@@ -0,0 +1,181 @@
+Node 0 initialized.
+[0] Running EDT with GUID: 18446744073692774401
+---------- Main EDT ----------
+---------- Compute EDT ----------
+[0] Signal DBGuid: 16777225 to EDTGuid 67108865 in slot 0
+[0] Signal DBGuid: 33554441 to EDTGuid 67108865 in slot 1
+[0] Signal DBGuid: 50331657 to EDTGuid 67108865 in slot 2
+[0] Signal DBGuid: 67108873 to EDTGuid 67108865 in slot 3
+[0] Signal DBGuid: 83886089 to EDTGuid 67108865 in slot 4
+[0] Signal DBGuid: 100663305 to EDTGuid 67108865 in slot 5
+[0] Signal DBGuid: 117440521 to EDTGuid 67108865 in slot 6
+[0] Signal DBGuid: 134217737 to EDTGuid 67108865 in slot 7
+[0] Signal DBGuid: 150994953 to EDTGuid 67108865 in slot 8
+[0] Signal DBGuid: 167772169 to EDTGuid 67108865 in slot 9
+[0] Running EDT with GUID: 68786585601
+---------- Parallel EDT started ----------
+[0] Signal DBGuid: 16777225 to EDTGuid 3539992577 in slot 0
+[0] Signal DBGuid: 100663305 to EDTGuid 3556769793 in slot 0
+[0] [0] Signal DBGuid: 33554441 to EDTGuid 3573547009 in slot 0
+[0] Signal DBGuid: 117440521 to EDTGuid 3590324225 in slot 0
+[0] Running EDT with GUID: 15191436295999062017
+------------------------
+--- Compute A[0] = 0.000000 - Guid: 16777225
+------------------------
+[0] Signal DBGuid: 16777225 to EDTGuid 3556769793 in slot 1
+[0] Signal DBGuid: 16777225 to EDTGuid 3590324225 in slot 2
+[0] EDT 15191436295999062017 Finished
+[0] Releasing 16777225[0] [0]  - is ARTS_DB_PIN: Skipping...
+Signal DBGuid: 50331657 to EDTGuid 3607101441 in slot 0
+[0] Signal DBGuid: 134217737 to EDTGuid 3623878657 in slot 0
+[0] Running EDT with GUID: 15191436296066170881
+[0] Signal DBGuid: 67108873 to EDTGuid 3640655873 in slot 0
+[0] Signal DBGuid: 150994953 to EDTGuid 3657433089 in slot 0
+[0] Running EDT with GUID: 15191436296099725313
+[0] Signal DBGuid: 83886089 to EDTGuid 3674210305 in slot 0
+[0] Running EDT with GUID: 15191436296015839233
+Running EDT with GUID: 15191436296032616449
+------------------------
+--- Compute B[0] = 0.000000
+------------------------
+[0] EDT 15191436296015839233 Finished
+------------------------
+--- Compute A[2] = 4.000000 - Guid: 50331657
+------------------------
+------------------------
+--- Compute A[3] = 6.000000 - Guid: 67108873
+------------------------
+[0] Signal DBGuid: 67108873 to EDTGuid 3657433089 in slot 1
+------------------------
+--- Compute A[1] = 2.000000 - Guid: 33554441
+------------------------
+[0] Signal DBGuid: 33554441 to EDTGuid 3590324225 in slot 1
+[0] Signal DBGuid: 33554441 to EDTGuid 3623878657 in slot 2
+[0] Signal DBGuid: 67108873 to EDTGuid 3690987521 in slot 2
+[0] EDT 15191436296099725313 Finished
+[0] Releasing 67108873[0] EDT 15191436296032616449 Finished
+[0] Releasing 33554441[0]  - is ARTS_DB_PIN: Skipping...
+[0] [0]  - is ARTS_DB_PIN: Skipping...
+Releasing 100663305[0] [0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 16777225[0]  - is ARTS_DB_PIN: Skipping...
+[0] Signal DBGuid: 167772169 to EDTGuid 3690987521 in slot 0
+[0] EDT 68786585601 Finished
+[0] Releasing 16777225Running EDT with GUID: 15191436296049393665
+------------------------
+--- Compute B[1] = 2.000000
+------------------------
+[0] EDT 15191436296049393665 Finished
+[0] Releasing 117440521[0] Running EDT with GUID: 15191436296133279745
+[0] [0] Signal DBGuid: 50331657 to EDTGuid 3623878657 in slot 1
+ - is ARTS_DB_PIN: Skipping...
+------------------------
+--- Compute A[4] = 8.000000 - Guid: 83886089
+------------------------
+[0] Signal DBGuid: 83886089 to EDTGuid 3690987521 in slot 1
+[0] EDT 15191436296133279745 Finished
+[0] [0] Running EDT with GUID: 15191436296150056961
+Releasing 83886089------------------------
+--- Compute B[4] = 14.000000
+------------------------
+[0] EDT 15191436296150056961 Finished
+[0] Releasing 167772169[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 83886089[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 67108873[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 33554441[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 50331657[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 67108873[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 83886089[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 100663305[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 117440521[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 134217737[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 150994953[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 167772169[0]  - is ARTS_DB_PIN: Skipping...
+[0] Signal DBGuid: 50331657 to EDTGuid 3657433089 in slot 2
+[0] EDT 15191436296066170881 Finished
+[0] [0] [0] Running EDT with GUID: 15191436296116502529
+[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 50331657[0]  - is ARTS_DB_PIN: Skipping...
+------------------------
+--- Compute B[3] = 10.000000
+------------------------
+[0] EDT 15191436296116502529 Finished
+[0] Releasing 150994953[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 67108873[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 50331657[0]  - is ARTS_DB_PIN: Skipping...
+ - is ARTS_DB_PIN: Skipping...
+[0] Releasing 33554441[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 16777225[0]  - is ARTS_DB_PIN: Skipping...
+Running EDT with GUID: 15191436296082948097
+------------------------
+--- Compute B[2] = 6.000000
+------------------------
+[0] Signal Value: 12 to EDTGuid 50331649 in slot 0
+[0] Running EDT with GUID: 68769808385
+[0] ---------- Parallel EDT finished ----------
+[0] EDT 68769808385 Finished
+[0] Releasing 12[0]  - is ARTS_DB_PIN: Skipping...
+EDT 15191436296082948097 Finished
+[0] Releasing 134217737[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 50331657[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 33554441[0]  - is ARTS_DB_PIN: Skipping...
+[0] Signal DBGuid: 16777225 to EDTGuid 83886081 in slot 0
+[0] Signal DBGuid: 33554441 to EDTGuid 83886081 in slot 1
+[0] Signal DBGuid: 50331657 to EDTGuid 83886081 in slot 2
+[0] Signal DBGuid: 67108873 to EDTGuid 83886081 in slot 3
+[0] Signal DBGuid: 83886089 to EDTGuid 83886081 in slot 4
+[0] Signal DBGuid: 100663305 to EDTGuid 100663297 in slot 0
+[0] Signal DBGuid: 117440521 to EDTGuid 100663297 in slot 1
+[0] Signal DBGuid: 134217737 to EDTGuid 100663297 in slot 2
+[0] Signal DBGuid: 150994953 to EDTGuid 100663297 in slot 3
+[0] Signal DBGuid: 167772169 to EDTGuid 100663297 in slot 4
+---------- Compute EDT finished ----------
+[0] Running EDT with GUID: 68820140033
+B[0]: 0.000000
+B[1]: 2.000000
+B[2]: 6.000000
+B[3]: 10.000000
+B[4]: 14.000000
+[0] EDT 68820140033 Finished
+[0] Releasing 100663305[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 117440521[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 134217737[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 150994953[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 167772169[0]  - is ARTS_DB_PIN: Skipping...
+[0] Running EDT with GUID: 68803362817
+A[0]: 0.000000
+A[1]: 2.000000
+A[2]: 4.000000
+A[3]: 6.000000
+A[4]: 8.000000
+[0] Signal Value: 4 to EDTGuid 16777217 in slot 0
+[0] EDT 68803362817 Finished
+[0] Releasing 16777225[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 33554441[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 50331657[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 67108873[0]  - is ARTS_DB_PIN: Skipping...
+[0] Releasing 83886089[0]  - is ARTS_DB_PIN: Skipping...
+---------- Main EDT finished ----------
+[0] EDT 18446744073692774401 Finished
+[0] Running EDT with GUID: 16777217
+--------------------------------------
+The program has finished its execution
+--------------------------------------
+[0] EDT 16777217 Finished
+[0] [0] [0] [0] [0] [0] [0] 5 joined: 0 threads
+14 joined: 0 threads
+[0] 10 joined: 0 threads
+[0] 1 joined: 0 threads
+[0] 9 joined: 0 threads
+8 joined: 0 threads
+15 joined: 0 threads
+[0] [0] 2 joined: 0 threads
+Releasing 44 joined: 0 threads
+11 joined: 0 threads
+[0] 12 joined: 0 threads
+3 joined: 0 threads
+[0] 6 joined: 0 threads
+[0] 13 joined: 0 threads
+[0]  - is ARTS_DB_PIN: Skipping...
+[0] 7 joined: 0 threads
+[0] 0 joined: 0 threads
+[0] Cleaned 0 bytes
--- examples/taskwithdepsfor/ideal/taskwithdeps_arts.c
+++ examples/taskwithdepsfor/ideal/taskwithdeps_arts.c
@@ -1,61 +1,67 @@
-
-
-// clang taskwithdeps_arts.c -O3 -g0 -march=native -I/home/randres/projects/carts/.install/arts/include -o taskwithdeps_arts -L/home/randres/projects/carts/.install/arts/lib -larts -lrdmacm
+// clang taskwithdeps_arts.c -O3 -g0 -march=native
+// -I/home/randres/projects/carts/.install/arts/include -o taskwithdeps_arts
+// -L/home/randres/projects/carts/.install/arts/lib -larts -lrdmacm
 #include "arts.h"
 #include "artsRT.h"
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// Task 1: Compute A[i]
 void computeA(uint32_t paramc, uint64_t *paramv, uint32_t depc,
               artsEdtDep_t depv[]) {
   // paramv[0] = i, paramv[1] = A[i] eventGuid
   unsigned int i = (unsigned int)paramv[0];
-  artsGuid_t eventGuid = (artsGuid_t)paramv[12];
+  artsGuid_t eventGuid = (artsGuid_t)paramv[1];
 
   // Access the DataBlock for A[i]
   double *A_i = (double *)depv[0].ptr;
+  artsGuid_t A_i_guid = depv[0].guid;
 
   // Compute A[i]
-  A_i[0] = i * 1.0;
+  A_i[0] = i * 2.0;
 
+  printf("------------------------\n"
+         "--- Compute A[%u] = %f - Guid: "
+         "%lu\n------------------------\n",
+         i, A_i[0], A_i_guid);
   // Signal the event associated with A[i] after computation
-  artsEventSatisfySlot(eventGuid, depv[0].guid, 0);
+  artsEventSatisfySlot(eventGuid, A_i_guid, ARTS_EVENT_LATCH_DECR_SLOT);
 }
 
-// Task 2: Compute B[i]
 void computeB(uint32_t paramc, uint64_t *paramv, uint32_t depc,
               artsEdtDep_t depv[]) {
-  // paramv[0] = i, paramv[1] = B[i] dbGuid
   unsigned int i = (unsigned int)paramv[0];
 
   // Access the DataBlocks for A[i], A[i-1] (via events), and B[i]
-  double *A_i = (double *)depv[0].ptr;
+  double *A_i = (double *)depv[1].ptr;
   double A_im1_val = 0.0;
   if (i > 0) {
-    double *A_im1 = (double *)depv[1].ptr;
+    double *A_im1 = (double *)depv[2].ptr;
     A_im1_val = A_im1[0];
   }
-  double *B_i = (double *)depv[2].ptr;
+  double *B_i = (double *)depv[0].ptr;
 
   // Compute B[i]
   B_i[0] = A_i[0] + A_im1_val;
+  printf("------------------------\n"
+         "--- Compute B[%u] = %f\n"
+         "------------------------\n",
+         i, B_i[0]);
 }
 
 void printDataBlockA(uint32_t paramc, uint64_t *paramv, uint32_t depc,
                      artsEdtDep_t depv[]) {
-  double *data = (double *)depv[0].ptr;
   for (int i = 0; i < paramv[0]; i++) {
-    printf("A[%d]: %f\n", i, data[i]);
+    double *data = (double *)depv[i].ptr;
+    printf("A[%d]: %f\n", i, *data);
   }
 }
 
 void printDataBlockB(uint32_t paramc, uint64_t *paramv, uint32_t depc,
                      artsEdtDep_t depv[]) {
-  double *data = (double *)depv[0].ptr;
   for (int i = 0; i < paramv[0]; i++) {
-    printf("B[%d]: %f\n", i, data[i]);
+    double *data = (double *)depv[i].ptr;
+    printf("B[%d]: %f\n", i, *data);
   }
 }
 
@@ -65,50 +71,46 @@ void parallelEdt(uint32_t paramc, uint64_t *paramv, uint32_t depc,
   printf("---------- Parallel EDT started ----------\n");
   /// Context
   unsigned int currentNode = artsGetCurrentNode();
-  artsGuid_t epochGuid = artsGetCurrentEpochGuid();
-  artsArrayDb_t *A_array = (artsArrayDb_t *)depv[0].ptr;
-  artsArrayDb_t *B_array = (artsArrayDb_t *)depv[1].ptr;
+  artsGuid_t curentEpoch = artsGetCurrentEpochGuid();
 
   /// Parameters
   int N = paramv[0];
-  artsGatherArrayDb(A_array, printDataBlockA, 0, 1, paramv, 0);
-  artsGatherArrayDb(B_array, printDataBlockB, 0, 1, paramv, 0);
+
+  /// Convert from depv to DataBlocks
+  artsDataBlock A_array[N], B_array[N];
+  artsDbCreateArrayFromDeps(A_array, N, depv, 0);
+  artsDbCreateArrayFromDeps(B_array, N, depv, N);
 
   // Preallocate all DataBlocks and events
   artsGuid_t *A_event = (artsGuid_t *)artsMalloc(N * sizeof(artsGuid_t));
+  for (int i = 0; i < N; i++)
+    A_event[i] = artsEventCreate(currentNode, 1);
+
+  // Create tasks for A[i] and B[i]
   for (int i = 0; i < N; i++) {
-    A_event[i] = artsEventCreate(currentNode, 1); // latchCount = 1
+    // Task 1: Compute A[i]
+    uint64_t Aparams[] = {(uint64_t)i, (uint64_t)A_event[i]};
+    artsGuid_t A_edt = artsEdtCreateWithEpoch((artsEdt_t)computeA, currentNode,
+                                              2, Aparams, 1, curentEpoch);
+
+    // Task 2: Compute B[i]
+    uint64_t Bparams[] = {(uint64_t)i};
+    // Dependencies: A[i], (A[i-1] if i > 0), and B[i]
+    int depCount = (i > 0) ? 3 : 2;
+    artsGuid_t B_edt = artsEdtCreateWithEpoch(
+        (artsEdt_t)computeB, currentNode, 2, Bparams, depCount, curentEpoch);
+
+    // Add dependencies to B[i] EDT through events
+    // Dependency: A[i]
+    artsAddDependence(A_event[i], B_edt, 1);
+    // Dependency: A[i-1]
+    if (i > 0)
+      artsAddDependence(A_event[i - 1], B_edt, 2);
+
+    /// Signal values after dependencies are added
+    artsSignalEdt(A_edt, 0, A_array[i].guid);
+    artsSignalEdt(B_edt, 0, B_array[i].guid);
   }
-  // // Create tasks for A[i] and B[i]
-  // for (int i = 0; i < N; i++) {
-  //   // Task 1: Compute A[i]
-  //   uint64_t Aparams[] = {(uint64_t)i, (uint64_t)A_event[i]};
-  //   artsGuid_t A_edt = artsEdtCreateWithEpoch((artsEdt_t)computeA,
-  //   currentNode,
-  //                                             2, Aparams, 1, epochGuid);
-  //   artsGetFromArrayDb(A_edt, 0, A_array, i);
-
-  //   // Task 2: Compute B[i]
-  //   uint64_t Bparams[] = {(uint64_t)i};
-  //   // Dependencies: A[i], (A[i-1] if i > 0), and B[i]
-  //   int depCount = (i > 0) ? 3 : 2;
-
-  //   artsGuid_t B_edt = artsEdtCreateWithEpoch((artsEdt_t)computeB,
-  //   currentNode,
-  //                                             2, Bparams, depCount,
-  //                                             epochGuid);
-
-  //   // Add dependencies to B[i] EDT through events
-  //   // Dependency: A[i]
-  //   artsAddDependence(A_event[i], B_edt, 0);
-  //   if (i > 0) {
-  //     // Dependency: A[i-1]
-  //     artsAddDependence(A_event[i - 1], B_edt, 1);
-  //   }
-  //   artsGetFromArrayDb(B_edt, 2, B_array, i);
-  //   // artsSignalEdt(B_edt, depCount - 1, B_db[i]); // Dependency: B[i]
-  //   // DataBlock
-  // }
 }
 
 void parallelDoneEdt(uint32_t paramc, uint64_t *paramv, uint32_t depc,
@@ -116,11 +118,11 @@ void parallelDoneEdt(uint32_t paramc, uint64_t *paramv, uint32_t depc,
   printf("---------- Parallel EDT finished ----------\n");
 }
 
-void computeEDT(int N, artsArrayDb_t *A_array, artsArrayDb_t *B_array) {
+void computeEDT(int N, artsDataBlock *A_array, artsDataBlock *B_array) {
   printf("---------- Compute EDT ----------\n");
-  // Context
+  /// Context
   unsigned int currentNode = artsGetCurrentNode();
-  artsGuid_t epochGuid = artsGetCurrentEpochGuid();
+  artsGuid_t curentEpoch = artsGetCurrentEpochGuid();
 
   /// Create parallel EDT
   artsGuid_t parallelDoneEdtGuid =
@@ -128,33 +130,31 @@ void computeEDT(int N, artsArrayDb_t *A_array, artsArrayDb_t *B_array) {
   artsGuid_t parallelDoneEpochGuid =
       artsInitializeAndStartEpoch(parallelDoneEdtGuid, 0);
 
-  // unsigned int workers = artsGetTotalWorkers();
-  // uint64_t parallelParams[1] = {(uint64_t)N};
-  // for(unsigned int i=0; i<workers; i++) {
-  //   artsGuid_t parallelEdtGuid = artsEdtCreateWithEpoch(
-  //     (artsEdt_t)parallelEdt, i, 1, parallelParams, 0,
-  //     parallelDoneEpochGuid);
-  //   artsSignalEdt(parallelEdtGuid, 0, NULL_GUID);
-  // }
-
-  /// paramv is N
-  // uint64_t paramv[1] = {(uint64_t)N};
-  // artsGatherArrayDbEpoch(A_array, printDataBlockA, 0, 1, paramv, 0, epochGuid);
-  // artsGatherArrayDbEpoch(B_array, printDataBlockB, 0, 1, paramv, 0, epochGuid);
-
   /// As of now, let's create only one instance of parallelEdt
   uint64_t parallelParams[1] = {(uint64_t)N};
-  uint64_t parallelDeps = 2;
+  uint64_t parallelDeps = 2 * N;
   artsGuid_t parallelEdtGuid = artsEdtCreateWithEpoch(
       (artsEdt_t)parallelEdt, currentNode, 1, parallelParams, parallelDeps,
       parallelDoneEpochGuid);
 
   /// Signal the dependencies
-  artsSignalArrayDb(A_array, parallelEdtGuid, 0);
-  artsSignalArrayDb(B_array, parallelEdtGuid, 1);
+  artsSignalDbs(A_array, parallelEdtGuid, 0, N);
+  artsSignalDbs(B_array, parallelEdtGuid, N, N);
 
-  /// Wait for the epoch to finish
+  /// Wait for the parallel epoch to finish
   artsWaitOnHandle(parallelDoneEpochGuid);
+
+  /// Print the arrays
+  uint64_t paramv[1] = {(uint64_t)N};
+  /// Print A
+  artsGuid_t printAEdtGuid = artsEdtCreateWithEpoch(
+      (artsEdt_t)printDataBlockA, currentNode, 1, paramv, N, curentEpoch);
+  artsSignalDbs(A_array, printAEdtGuid, 0, N);
+  /// Print B
+  artsGuid_t printBEdtGuid = artsEdtCreateWithEpoch(
+      (artsEdt_t)printDataBlockB, currentNode, 1, paramv, N, curentEpoch);
+  artsSignalDbs(B_array, printBEdtGuid, 0, N);
+
   printf("---------- Compute EDT finished ----------\n");
 }
 
@@ -178,7 +178,7 @@ void mainEdt(uint32_t paramc, uint64_t *paramv, uint32_t depc,
       artsInitializeAndStartEpoch(finishMainEdtGuid, 0);
 
   /// Define the size of the computation
-  int N = 10;
+  int N = 5;
 
   // Allocate arrays for DataBlocks and events
   double *A = (double *)artsMalloc(N * sizeof(double));
@@ -190,19 +190,18 @@ void mainEdt(uint32_t paramc, uint64_t *paramv, uint32_t depc,
   }
 
   // Create DataBlocks for A and B
-  artsGuid_t A_guid = artsReserveGuidRoute(ARTS_DB_PIN, currentNode);
-  artsGuid_t B_guid = artsReserveGuidRoute(ARTS_DB_PIN, currentNode);
-  artsArrayDb_t *A_array =
-      artsNewLocalArrayDbWithGuid(A_guid, sizeof(double), N, (void *)A);
-  artsArrayDb_t *B_array =
-      artsNewLocalArrayDbWithGuid(B_guid, sizeof(double), N, (void *)B);
+  artsDataBlock A_array[N], B_array[N];
+  artsDbCreateArray(A_array, sizeof(double), ARTS_DB_PIN, N, (void *)A);
+  artsDbCreateArray(B_array, sizeof(double), ARTS_DB_PIN, N, (void *)B);
 
-  /// We dont need the arrays anymore
-  // artsFree(A);
-  // artsFree(B);
-
+  // Call the computeEDT
   computeEDT(N, A_array, B_array);
-  // artsWaitOnHandle(finishMainEpochGuid);
+
+  // Wait for the finish EDT to end the epoch
+  artsWaitOnHandle(finishMainEpochGuid);
+  // We dont need the arrays anymore
+  artsFree(A);
+  artsFree(B);
   printf("---------- Main EDT finished ----------\n");
 }
 
