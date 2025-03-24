590d74d97194bc2f17527d5e9201a1a041f9ec7f
randreshg <randres2011@gmail.com>
2025-03-24 12:46:55 -0400

    Version for the tutorial - 1

--- examples/simple/simple.cpp
+++ examples/simple/simple.cpp
@@ -12,23 +12,23 @@ int main() {
   srand(time(NULL));
   int shared_number = rand() % 100 + 1;
   int random_number = rand() % 10 + 1;
-  printf("EDT 1: The initial number is %d/%d\n", shared_number, random_number);
+  printf("EDT 0: The initial number is %d/%d\n", shared_number, random_number);
 
   #pragma omp parallel
   {
     #pragma omp single
     {
-      printf("EDT 0: The number is %d/%d\n", shared_number, random_number);
+      printf("EDT 1: The number is %d/%d\n", shared_number, random_number);
       #pragma omp task firstprivate(random_number)
       {
         shared_number++;
         random_number++;
-        printf("EDT 3: The number is %d/%d\n", shared_number, random_number);
+        printf("EDT 2: The number is %d/%d\n", shared_number, random_number);
       }
     }
   } 
 
   shared_number++;
-  printf("EDT 2: The final number is %d - %d.\n", shared_number, random_number);
+  printf("EDT 3: The final number is %d - %d.\n", shared_number, random_number);
   return 0;
 }
