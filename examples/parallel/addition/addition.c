1abe978e1e2a843d567d4919d70b6b9993835c44
randreshg <randres2011@gmail.com>
2025-05-08 18:20:49 -0400

    Refactor addition example to accept dynamic array size and improve error handling; enhance output with thread information

--- examples/parallel/addition/addition.c
+++ examples/parallel/addition/addition.c
@@ -2,31 +2,46 @@
 #include <stdio.h>
 
 /*
-cgeist matrixmul.c -fopenmp -O0 -S -I/usr/lib/llvm-14/lib/clang/14.0.0/include > matrixmul.mlir
+cgeist addition.c -fopenmp -O0 -S -I/usr/lib/llvm-14/lib/clang/14.0.0/include > addition.mlir
 
-carts-opt matrixmul.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --edt --edt-invariant-code-motion --canonicalize-polygeist --create-datablocks --canonicalize-polygeist --datablock --cse -debug-only=datablock,datablock-analysis,create-datablocks &> matrixmul-arts.mlir
+carts-opt addition.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --canonicalize-polygeist --datablock --cse -debug-only=convert-openmp-to-arts &> addition-arts.mlir
 
-carts-opt matrixmul.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --edt --edt-invariant-code-motion --canonicalize-polygeist --create-datablocks --canonicalize-polygeist --datablock --cse -debug-only=edt-invariant-code-motion,datablock,create-datablocks &> matrixmul-arts.mlir
+carts-opt addition.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --edt --edt-invariant-code-motion --canonicalize-polygeist --create-datablocks --canonicalize-polygeist --datablock --cse -debug-only=datablock,datablock-analysis,create-datablocks &> addition-arts.mlir
 
-carts-opt matrixmul.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --edt --edt-invariant-code-motion --canonicalize-polygeist --create-datablocks --cse --polygeist-mem2reg --canonicalize-polygeist --datablock --cse --canonicalize-scf-for --canonicalize-polygeist --polygeist-mem2reg --edt-pointer-rematerialization --create-events --create-epochs --canonicalize-polygeist --convert-arts-to-llvm --canonicalize-polygeist --cse --convert-polygeist-to-llvm --cse -debug-only=convert-arts-to-llvm &> matrixmul-arts.mlir
+carts-opt addition.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --edt --edt-invariant-code-motion --canonicalize-polygeist --create-datablocks --canonicalize-polygeist --datablock --cse -debug-only=datablock,datablock-analysis,create-datablocks &> addition-arts.mlir
 
-mlir-translate --mlir-to-llvmir matrixmul-arts.mlir &> matrixmul-arts.ll
+carts-opt addition.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --edt --edt-invariant-code-motion --canonicalize-polygeist --create-datablocks --canonicalize-polygeist --datablock --cse -debug-only=edt-invariant-code-motion,datablock,create-datablocks &> addition-arts.mlir
 
-clang matrixmul-arts.ll -O3 -g0 -march=native -o matrixmul -I/home/randres/projects/carts/.install/arts/include -L/home/randres/projects/carts/.install/arts/lib -larts -L/usr/lib64/librt.so/usr/lib64/libpthread.so/usr/lib64/lib -lrdmacm
+carts-opt addition.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --edt --edt-invariant-code-motion --canonicalize-polygeist --create-datablocks --cse --polygeist-mem2reg --canonicalize-polygeist --datablock --cse --canonicalize-scf-for --canonicalize-polygeist --polygeist-mem2reg --edt-pointer-rematerialization --create-events --create-epochs --canonicalize-polygeist --cse -debug-only=convert-arts-to-llvm &> addition-arts.mlir
+
+
+carts-opt addition.mlir --lower-affine --cse --polygeist-mem2reg --canonicalize-polygeist --loop-invariant-code-motion --canonicalize-polygeist --convert-openmp-to-arts --edt --edt-invariant-code-motion --canonicalize-polygeist --create-datablocks --cse --polygeist-mem2reg --canonicalize-polygeist --datablock --cse --canonicalize-scf-for --canonicalize-polygeist --polygeist-mem2reg --edt-pointer-rematerialization --create-events --create-epochs --canonicalize-polygeist --convert-arts-to-llvm --canonicalize-polygeist --cse --convert-polygeist-to-llvm --cse -debug-only=convert-arts-to-llvm &> addition-arts.mlir
+
+mlir-translate --mlir-to-llvmir addition-arts.mlir &> addition-arts.ll
+
+clang addition-arts.ll -O3 -g0 -march=native -o addition -I/home/randres/projects/carts/.install/arts/include -L/home/randres/projects/carts/.install/arts/lib -larts -L/usr/lib64/librt.so/usr/lib64/libpthread.so/usr/lib64/lib -lrdmacm
 */
 
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define N 1000
 
-int main() {
+int main(int argc, char **argv) {
+  if(argc < 2) {
+    printf("Usage: %s <num_threads>\n", argv[0]);
+    return 1;
+  }
+  const int N = atoi(argv[1]);
+  if(N <= 0) {
+    printf("Error: N must be a positive integer.\n");
+    return 1;
+  }
   int a[N], b[N], c[N];
-  int i;
 
   /// Initialize arrays
-  for (i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++) {
     a[i] = i;
     b[i] = i * 2;
   }
@@ -35,18 +50,32 @@ int main() {
 
   /// Parallel loop for array addition
   #pragma omp parallel for
-  for (i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++) {
+    printf("Thread %d is working on iteration %d\n", omp_get_thread_num(), i);
     c[i] = a[i] + b[i];
   }
 
   printf("Parallel addition finished.\n");
   /// Verify results
-  for (i = 0; i < N; i++) {
-    if (c[i] != a[i] + b[i]) {
-      printf("Error at index %d: expected %d, got %d\n", i, a[i] + b[i], c[i]);
-      return 1;
+  bool success = true;
+  for (int i = 0; i < N; i++) {
+    if (c[i] != (a[i] + b[i])) {
+      success = false;
     }
   }
+
+  if (!success) {
+    printf("Array addition failed.\n");
+  } else {
+    printf("Array addition succeeded.\n");
+  }
+
+  /// Print C
+  printf("C array: ");
+  for (int i = 0; i < N; i++) {
+    printf("%d ", c[i]);
+  }
+  printf("\n");
 
 
   return 0;
