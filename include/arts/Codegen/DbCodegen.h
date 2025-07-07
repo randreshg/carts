2ff3c4c48a90d80c69d8b00ab1bf2f4be461b52b
randreshg <randres2011@gmail.com>
2025-07-06 22:08:53 -0400

    simple example compiles

--- include/arts/Codegen/DbCodegen.h
+++ include/arts/Codegen/DbCodegen.h
@@ -4,11 +4,11 @@
 #ifndef CARTS_CODEGEN_DBCODEGEN_H
 #define CARTS_CODEGEN_DBCODEGEN_H
 
-#include "mlir/IR/Builders.h"
-#include "mlir/IR/Value.h"
-#include "mlir/IR/Types.h"
-#include "mlir/IR/Location.h"
 #include "arts/ArtsDialect.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/Location.h"
+#include "mlir/IR/Types.h"
+#include "mlir/IR/Value.h"
 #include <vector>
 
 namespace mlir {
@@ -17,11 +17,12 @@ namespace arts {
 class ArtsCodegen;
 
 /// DbAllocCodegen - Handles datablock allocation (local creation)
-/// No EDT context needed - when used in EDT context, consumer sees it as DbDepOp
+/// No EDT context needed - when used in EDT context, consumer sees it as
+/// DbDepOp
 class DbAllocCodegen {
 public:
   DbAllocCodegen(ArtsCodegen &AC, arts::DbAllocOp dbOp, Location loc);
-  
+
   Value getOp();
   Value getEdtSlot();
   Value getGuid();
@@ -42,23 +43,28 @@ private:
   Value edtSlot = nullptr;
   Value guid = nullptr;
   Value ptr = nullptr;
-  
+
   Value createGuid(Value node, Value mode, Location loc);
   Value getMode();
   Value calculateElementSize(Location loc);
 };
 
-/// DbDepCodegen - Handles datablock dependencies (accessing existing DBs from depv)
-/// 
+/// DbDepCodegen - Handles datablock dependencies (accessing existing DBs from
+/// depv)
+///
 /// EDT Context Semantics:
-/// - "InEdt" means in the EDT that USES/CONSUMES the datablock, not the EDT that creates it
-/// - When a DbAllocOp is used in an EDT context, it's ALWAYS accessed through a DbDepOp  
+/// - "InEdt" means in the EDT that USES/CONSUMES the datablock, not the EDT
+/// that creates it
+/// - When a DbAllocOp is used in an EDT context, it's ALWAYS accessed through a
+/// DbDepOp
 /// - The consumer EDT sees it as a DbDepOp, not a DbAllocOp
-/// - This class contains all EDT context information for accessing datablocks from depv parameters
+/// - This class contains all EDT context information for accessing datablocks
+/// from depv parameters
 class DbDepCodegen {
 public:
-  DbDepCodegen(ArtsCodegen &AC, arts::DbDepOp dbOp, Location loc);
-  
+  DbDepCodegen(ArtsCodegen &AC, arts::DbDepOp dbOp,
+               Operation *parentOp = nullptr);
+
   Value getOp();
   Value getEdtSlot();
   Value getGuid();
@@ -72,7 +78,10 @@ public:
   ValueRange getSizes();
   ValueRange getOffsets();
   ValueRange getIndices();
-  void create(Location loc);
+  void init(Location loc);
+
+  /// Parent op
+  bool isParentOpInSameEdt();
 
   /// EDT context values (set once during EDT entry construction)
   Value getGuidInEdt() { return guidInEdt; }
@@ -83,8 +92,12 @@ public:
   /// EDT context information for accessing existing DBs from depv
   llvm::SmallVector<Value> &getSizesInEdt() { return sizesInEdt; }
   llvm::SmallVector<Value> &getOffsetsInEdt() { return offsetsInEdt; }
-  llvm::DenseMap<unsigned, unsigned> &getSizeIndexInEdt() { return sizeIndexInEdt; }
-  llvm::DenseMap<unsigned, unsigned> &getOffsetIndexInEdt() { return offsetIndexInEdt; }
+  llvm::DenseMap<unsigned, unsigned> &getSizeIndexInEdt() {
+    return sizeIndexInEdt;
+  }
+  llvm::DenseMap<unsigned, unsigned> &getOffsetIndexInEdt() {
+    return offsetIndexInEdt;
+  }
 
 private:
   ArtsCodegen &AC;
@@ -93,10 +106,15 @@ private:
   Value edtSlot = nullptr;
   Value guid = nullptr;
   Value ptr = nullptr;
-  
-  /// EDT context values - for accessing existing datablocks from depv parameters
-  Value guidInEdt = nullptr;  // GUID in EDT consumer context
-  Value ptrInEdt = nullptr;   // Pointer in EDT consumer context
+
+  /// Parent op
+  Operation *parentOp = nullptr;
+  bool parentOpIsInSameEdt = true;
+
+  /// EDT context values - for accessing existing datablocks from depv
+  /// parameters
+  Value guidInEdt = nullptr; // GUID in EDT consumer context
+  Value ptrInEdt = nullptr;  // Pointer in EDT consumer context
 
   /// EDT context data for multi-dimensional access from depv parameters
   llvm::SmallVector<Value> sizesInEdt;
@@ -108,4 +126,4 @@ private:
 } // namespace arts
 } // namespace mlir
 
-#endif // CARTS_CODEGEN_DBCODEGEN_H 
+#endif // CARTS_CODEGEN_DBCODEGEN_H
