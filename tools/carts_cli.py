38d5394a56ef1d36e4ac15c10066408028cd876d
Woohyeon Baek <whnbaek@gmail.com>
2026-01-10 01:24:55 +0900

    Apply suggestions from code review

--- tools/carts_cli.py
+++ tools/carts_cli.py
@@ -253,6 +253,9 @@ class PlatformConfig:
 
         self.clang_library_flags.extend(["-L/usr/lib", "-L/usr/lib64"])
         self.compile_library_flags.extend(["-L/usr/lib", "-L/usr/lib64"])
+        # Ensure standard Linux libraries are linked when needed.
+        self.clang_library_flags.extend(["-lpthread", "-lrt", "-lstdc++"])
+        self.compile_library_flags.extend(["-lpthread", "-lrt", "-lstdc++"])
         self.compile_flags.extend(["-fno-pie", "-no-pie"])
 
     def _setup_windows_flags(self) -> None:
