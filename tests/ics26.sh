a94ec683c441e457ef2484ca79e963a5969b9e5b
Woohyeon Baek <whnbaek@gmail.com>
2026-02-07 20:18:15 -0500

    Refactor benchmark scripts to use 'large' dataset size and adjust timing parameters

--- tests/ics26.sh
+++ tests/ics26.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
 
-# Main experiment (CARTS vs. OMP in single node) + multi-node scaling
-# carts benchmarks slurm-run \
-#     --nodes=1,2,4,8,16,32 \
-#     --arts-config=configs/arts.cfg \
-#     --counter-config=configs/profile-timing.cfg \
-#     --size=extralarge \
-#     --runs=5 \
-#     --time=00:02:00
-
 # Workload profiling with both ARTS counters and Linux HW counters
 carts benchmarks slurm-run \
     --nodes=1 \
     --arts-config=configs/arts.cfg \
     --counter-config=configs/profile-workload.cfg \
-    --size=extralarge \
+    --size=large \
     --runs=1 \
     --time=00:01:00 \
     --perf
@@ -24,46 +15,70 @@ carts benchmarks slurm-run \
     --nodes=1 \
     --arts-config=configs/arts.cfg \
     --counter-config=configs/profile-timing.cfg \
-    --size=extralarge \
+    --size=large \
     --runs=5 \
-    --time=00:00:30
+    --time=00:01:00
 
 carts benchmarks slurm-run \
     --nodes=1 \
     --arts-config=configs/arts-32T.cfg \
     --counter-config=configs/profile-timing.cfg \
-    --size=extralarge \
+    --size=large \
     --runs=5 \
-    --time=00:01:00 \
+    --time=00:02:00 \
 
 carts benchmarks slurm-run \
     --nodes=1 \
     --arts-config=configs/arts-16T.cfg \
     --counter-config=configs/profile-timing.cfg \
-    --size=extralarge \
-    --runs=5 \
-    --time=00:02:00
-
-carts benchmarks slurm-run \
-    --nodes=1 \
-    --arts-config=configs/arts-8T.cfg \
-    --counter-config=configs/profile-timing.cfg \
-    --size=extralarge \
+    --size=large \
     --runs=5 \
     --time=00:04:00
 
 carts benchmarks slurm-run \
     --nodes=1 \
-    --arts-config=configs/arts-4T.cfg \
+    --arts-config=configs/arts-8T.cfg \
     --counter-config=configs/profile-timing.cfg \
-    --size=extralarge \
+    --size=large \
     --runs=5 \
     --time=00:08:00
 
 carts benchmarks slurm-run \
     --nodes=1 \
+    --arts-config=configs/arts-4T.cfg \
+    --counter-config=configs/profile-timing.cfg \
+    --size=large \
+    --runs=5 \
+    --time=00:16:00
+
+carts benchmarks slurm-run \
+    --nodes=1 \
     --arts-config=configs/arts-2T.cfg \
+    --counter-config=configs/profile-timing.cfg \
+    --size=large \
+    --runs=5 \
+    --time=00:32:00
+
+# Main experiment (CARTS vs. OMP in single node) + multi-node scaling
+# In case, large dataset first
+carts benchmarks slurm-run \
+    --nodes=2,4,8,16,32 \
+    --arts-config=configs/arts.cfg \
+    --counter-config=configs/profile-timing.cfg \
+    --size=large \
+    --runs=5 \
+    --time=00:10:00 \
+    --exclude jacobi-task-dep \
+    --exclude poisson-task
+
+# Then extralarge dataset
+carts benchmarks slurm-run \
+    --nodes=1,2,4,8,16,32 \
+    --arts-config=configs/arts.cfg \
     --counter-config=configs/profile-timing.cfg \
     --size=extralarge \
     --runs=5 \
-    --time=00:16:00
+    --time=00:10:00 \
+    --exclude jacobi-task-dep \
+    --exclude poisson-task
+
